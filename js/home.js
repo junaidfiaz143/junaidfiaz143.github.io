bbb2c4a5052d522f7180f0a1ff8222849fbc8938
M. Junaid Fiaz <junaidfiaz143@gmail.com>
2023-05-15 13:13:05 +0900

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,50 @@
+
+function openURL(url) {
+	window.open(url, '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function otsukalab_onedrive() {
+	window.open('https://nitechict-my.sharepoint.com/personal/cmw14115_ict_nitech_ac_jp/', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function calendar() {
+	window.open('https://calendar.google.com/calendar/u/0/r?pli=1', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function student_portal() {
+	window.open('https://www.cc.nitech.ac.jp/stn-portal-login.html', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function moodle() {
+	window.open('https://cms6.ict.nitech.ac.jp/moodle38a/my', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function deep_l() {
+	window.open('https://www.deepl.com/en/translator', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function google_translate() {
+	window.open('https://translate.google.com', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function otsukalab() {
+	window.open('http://otsukalab.web.nitech.ac.jp/', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function my_tally_ui() {
+	$('#exampleModal').modal('show');
+
+	// window.open('https://www.figma.com/file/8mHQgkXdoA2c4jMSFH0yle/My-Tally?node-id=17%3A3', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function my_expense() {
+	window.open('https://nitech.odoo.com/web#cids=1&action=menu', '_blank', 'location=yes,scrollbars=yes,status=yes');
+}
+
+function nihongo_jisho() {
+	window.open('https://jisho.org/', '_blank', 'location=yes, scrollbars=yes, status=yes')
+}
+
+function campus_square() {
+	window.open('https://course.web.nitech.ac.jp', '_blank', 'location=yes, scrollbars=yes, status=yes');
+}
